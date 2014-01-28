423e74dbc82c185a890a91743357445b5e4daf5f
Ubuntu <ubuntu@ip-172-31-40-20.us-west-2.compute.internal>
2014-01-28 17:29:19 +0000

    modified:   web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var data = fs.readFileSync(path);
+    //var data = fs.readFileSync(path);
     //response.send(data.toString());
     response.send("hello");
 });
