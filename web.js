4ddc517cfefa97f4716de83ba806b4e0004fe693
Ubuntu <ubuntu@ip-172-31-40-20.us-west-2.compute.internal>
2014-01-28 17:18:52 +0000

    add index.html, change web.js to display the content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('./index.html');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var data = fs.readFile(path);
+    response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
